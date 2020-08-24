95244a0dcb0509ce5cbaccacc57b64a2736d1344
Jhongtao <466795794@qq.com>
2020-08-24 13:42:11 +0800

    demo-1.1

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-console.log('index');
-let a = 1, b = 2;
-console.log(a + b)
-console.log((a / b).toFixed(4))
-console.log(b / a)
-module.exprots = function () {
-    return
+function randomNun() {
+    return Math.random()
 }
 
-=======
-console.log('index')
-exports.getBranch = () => {
-    return this.name
-}
-console.log(Math.random)
->>>>>>> demo
+export default randomNun
